b3d06d1f655902b8c86fe92403500882c2454544
Menelik Admasu <minilover2014@gmail.com>
2026-09-23 16:02:27 +0300

    Use real church photographs on the homepage

--- src/data/church-photos.ts
+++ src/data/church-photos.ts
@@ -1,4 +1,5 @@
-/** Real church photos provided by the organization */
-export const churchFront = "/images/church-front.svg";
-export const churchAerial = "/images/church-aerial.svg";
-export const christIcon = "/images/christ-icon.svg";
+/** Real photos of Debre Selam Beale Egziabher Church */
+export const churchFront = "/images/FB_IMG_1790157715112.jpg";
+export const churchAerial =
+  "/images/Screenshot_2026-09-23-13-20-01-55_a23b203fd3aafc6dcb84e438dda678b6.jpg";
+export const christIcon = "/images/FB_IMG_1790157715112.jpg";
